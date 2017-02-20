7679502888db02b15c511fdb4e2ae25d7a717eef
ayakovlenko <tony.yakovlenko@gmail.com>
2017-02-20 19:11:40 +0200

    Vect: Refactoring + projectOnto, cross

--- src/main/scala/v1/la/Vect.scala
+++ src/main/scala/v1/la/Vect.scala
@@ -1,6 +1,10 @@
 package v1.la
 
-case class Vect(override val seq: Seq[Double]) extends Iterable[Double] {
+import scala.collection.immutable.IndexedSeq
+
+case class Vect(iterables: Iterable[Double]) extends IndexedSeq[Double] {
+
+  override val seq: IndexedSeq[Double] = iterables.toIndexedSeq
 
   def +(that: Vect): Vect = {
     require(this.seq.length == that.seq.length, "addition of vectors of different length")
@@ -34,6 +38,10 @@ case class Vect(override val seq: Seq[Double]) extends Iterable[Double] {
     }
   }
 
+  def /(scalar: Double): Vect = {
+    Vect(seq map (_ / scalar))
+  }
+
   def magnitude: Double = math.sqrt((seq map (x => math.pow(x, 2))).sum)
 
   def unit: Vect = this * (1 / magnitude)
@@ -54,9 +62,35 @@ case class Vect(override val seq: Seq[Double]) extends Iterable[Double] {
     if (this.isZero || that.isZero) true else (this dot that) ~= (0, precision)
   }
 
+  /**
+    * Projects this vector onto base vector.
+    *
+    * @param base base vector
+    * @return a tuple of components parallel and orthonogal to base vector
+    */
+  def projectOnto(base: Vect): (Vect, Vect) = {
+    val parallel = base.unit * (this dot base.unit)
+    val orthogonal = this - parallel
+
+    (parallel, orthogonal)
+  }
+
+  def cross(that: Vect): Vect = {
+    require(this.length == 3 && that.length == 3)
+
+    val (x1, y1, z1) = (this(0), this(1), this(2))
+    val (x2, y2, z2) = (that(0), that(1), that(2))
+
+    Vect.of(
+      y1 * z2 - y2 * z1,
+      -(x1 * z2 - x2 * z1),
+      x1 * y2 - x2 * y1
+    )
+  }
+
   override def toString: String = seq.mkString("<[", ", ", "]>")
 
-  private val precision = 1e-14
+  private val precision = 1e-12
 
   private implicit class DoubleLike(x: Double) {
 
@@ -65,7 +99,11 @@ case class Vect(override val seq: Seq[Double]) extends Iterable[Double] {
     }
   }
 
+  override def length: Int = seq.length
+
   override def iterator: Iterator[Double] = seq.iterator
+
+  override def apply(idx: Int): Double = seq(idx)
 }
 
 object Vect {
